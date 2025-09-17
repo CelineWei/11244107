2dd986cd088cc115f2bec36602e5ff7070d2e370
Celine <celinewei520@gmail.com>
2025-09-17 22:10:17 +0800

    加入專案檔案。

---
+++ Console Application/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text; 
+using System.Threading.Tasks;
+
+namespace _0916ConsoleApplication
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("11244107");
+            Console.WriteLine("韋子晴");
+            Console.ReadKey();
+        }
+    }
+}
